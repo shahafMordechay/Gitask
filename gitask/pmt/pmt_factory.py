ea4851fddef88ae802c6fa9425f105e70910a3ac
Shahaf Mordehay <smordehay@infinidat.com>
2025-04-13 23:54:40 +0300

    Support github VCS and PMT

--- gitask/pmt/pmt_factory.py
+++ gitask/pmt/pmt_factory.py
@@ -1,10 +1,19 @@
 from gitask.config.config import Config
+from gitask.pmt.github_pmt import GitHubPmt
 from gitask.pmt.jira_pmt import JiraPmt
+from gitask.pmt.project_management_tool import PMToolInterface
 
 
-def get_pmt():
+def get_pmt() -> PMToolInterface:
+    """
+    Get the appropriate PMT implementation based on the configuration.
+    :return: The PMT implementation.
+    """
     pmt_type = Config().pmt_type.lower()
-
+    
     if pmt_type == "jira":
         return JiraPmt()
-    raise ValueError(f"Unsupported PMT type: {pmt_type}")
+    elif pmt_type == "github":
+        return GitHubPmt()
+    else:
+        raise ValueError(f"Unsupported PMT type: {pmt_type}")
